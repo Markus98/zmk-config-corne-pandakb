81c6f02ad19cc935a63b5ada488f63beb2b28549
Markus Tuominen <3738613+Markus98@users.noreply.github.com>
2025-08-18 12:33:17 +0300

    feat: move esc to thumb row

--- generate_keymap_image.sh
+++ generate_keymap_image.sh
@@ -3,8 +3,8 @@
 # A utility script to generate the keymap image for the layout
 # Requirest the keymap-drawer python utility to be installed
 
-keymap -c keymap_image/keymap_draw_config.yaml parse -c 12 -z config/corne.keymap > keymap_image/corne_keymap.yaml
-keymap -c keymap_image/keymap_draw_config.yaml draw keymap_image/corne_keymap.yaml > keymap_image/corne_keymap.svg
+keymap -c keymap_image/keymap_config.yaml parse -c 12 -z config/corne.keymap > keymap_image/corne_keymap.yaml
+keymap -c keymap_image/keymap_config.yaml draw keymap_image/corne_keymap.yaml > keymap_image/corne_keymap.svg
 magick -size 2160x3480 -background black keymap_image/corne_keymap.svg keymap_image/corne_keymap.jpg
 
 # Remove generated keymap yaml
